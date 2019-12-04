dbd2ad99dce166c3258ccad23ec3d93579ae7cf1
Fernando <marinhosilva.fernando@gmail.com>
2019-12-04 01:06:54 +0000

    homescreen done

--- www/js/api.js
+++ www/js/api.js
@@ -1,22 +1,20 @@
 var api = {
-    method: "GET",
-    fetchData: function (url) {
-        return new Promise((resolve, reject) => {
-            var http = new XMLHttpRequest();
-            console.log(this.method, url);
-            http.open(this.method, url);
-            http.send();
-            http.onreadystatechange = (e) => {
-                var response = http.responseText;
-                var responseJSON = JSON.parse(response);
-                resolve(responseJSON)
-            }
-        })
-    },
+  method: "GET",
+  fetchData: function(url) {
+    return new Promise((resolve, reject) => {
+      var http = new XMLHttpRequest();
+      console.log(this.method, url);
+      http.open(this.method, url);
+      http.send();
+      http.onreadystatechange = e => {
+        var response = http.responseText;
+        var responseJSON = JSON.parse(response);
+        resolve(responseJSON);
+      };
+    });
+  },
 
-    setMethod(method = "GET" | "POST") {
-        this.method = method;
-    }
-
-
-}
+  setMethod(method = "GET" | "POST") {
+    this.method = method;
+  }
+};
--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -1,97 +1,109 @@
 var geo = {
-    apiUrl: `https://api.opencagedata.com/geocode/v1/json?`,
-    api: undefined,
-    apiKey: "69ae4737763149a18be447eb47817ad8",
-    device: undefined,
-    // Application Constructor
-    initialize: function () {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+  apiUrl: `https://api.opencagedata.com/geocode/v1/json?`,
+  api: undefined,
+  apiKey: "69ae4737763149a18be447eb47817ad8",
+  device: undefined,
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener("deviceready", this.onDeviceReady, false);
+  },
 
-    onDeviceReady: function () {
-        (async () => {
-            var l = await geo.getLocationData();
-            var { country_code, country, city } = l.components
-            var element = document.getElementById('geolocation');
-            console.log(l)
-            element.innerHTML = `
-                <img class="" src="https://www.countryflags.io/${country_code}/shiny/64.png"> 
-                <p>${city}, ${country}</p>
-                <div class="pa4 tc">
-  <img
-      src="https://www.countryflags.io/${country_code}/shiny/64.png"
-      class="h3 w3 dib" alt="flag">
-      <p class="ma0">${city}, ${country}</p>
-</div>
-            `
-        })();
+  onDeviceReady: function() {
+    (async () => {
+      var l = await geo.getLocationData();
+      var { country_code, country, city } = l.components;
+      var callingCode = l.annotations.callingcode;
+      var { iso_code, name, symbol } = l.annotations.currency;
+      var { lat, lng } = l.geometry;
+      var element = document.getElementById("geolocation");
+      console.log(l);
+      element.innerHTML = `
+      <div class="ma0 center-align">
+        <img
+            src="https://www.countryflags.io/${country_code}/shiny/64.png"
+            class="h3 w3" alt="flag">
+        <p class="ma0">
+          ${city}, ${country}
+        </p>
+      <div class="divider mv4"></div>
+      </div>
+      <div class="mh2">
+        <h5>More info:</h5>
+        <div class="grey-text text-darken-3">
+        <p class="ma0">
+          <span class="black-text ">Country: </span>${country} (${country_code})
+        </p>
+        <p class="ma0">
+          <span class="black-text ">City: </span>${city}
+        </p>
+        <p class="ma0">
+          <span class="black-text ">Calling code: </span>${callingCode}
+        </p>
+        <p class="ma0">
+          <span class="black-text ">Currency name: </span>${name} - ${symbol}
+        </p>
+        <p class="ma0">
+          <span class="black-text ">Latitude: </span>${lat}, <span class="black-text ">Longitude: </span>${lng}
+        </p>
+        </div>
+      </div>`;
+    })();
+  },
+  setApi: function(api) {
+    this.api = api;
+  },
+  getLocationData: function() {
+    return (async () => {
+      let pos,
+        lat,
+        long = undefined;
+      if (device.platform !== "browser") {
+        pos = await getPosition();
+      } else {
+        //set default Location if in browser (Dublin)
+        pos = {
+          coords: {
+            latitude: 53.349804,
+            longitude: -6.26031
+          }
+        };
+      }
+      lat = pos.coords.latitude;
+      long = pos.coords.longitude;
+      if (!this.api || !pos || !this.apiKey) {
+        alert("Couldn't fetch location");
+        console.log(
+          `Api: ${!!this.api} \n`,
+          `Position: ${!!pos}\n`,
+          `ApiKey: ${this.apiKey}\n`
+        );
+        return undefined;
+      }
+      let url = this.apiUrl + `q=${lat}+${long}&key=${this.apiKey}`;
+      console.log(url);
+      let data = await this.api.fetchData(url);
 
-    },
-    setApi: function (api) {
-        this.api = api;
-    },
-    getLocationData: function () {
-        return (async () => {
-            let pos, lat, long = undefined;
-            if (device.platform !== "browser") {
-                pos = await getPosition();
-            } else {
-                //set default Location if in browser (Dublin)
-                pos = {
-                    coords: {
-                        latitude: 53.349804,
-                        longitude: -6.260310
-                    }
-                }
-            }
-            lat = pos.coords.latitude
-            long = pos.coords.longitude
-            if (!this.api || !pos || !this.apiKey) {
-                alert("Couldn't fetch location")
-                console.log(`Api: ${!!this.api} \n`,
-                    `Position: ${!!pos}\n`,
-                    `ApiKey: ${this.apiKey}\n`);
-                return undefined;
-            }
-            let url = this.apiUrl + `q=${lat}+${long}&key=${this.apiKey}`;
-            console.log(url);
-            let data = await this.api.fetchData(url)
-
-            return data.results[0];
-        })()
-    },
-
-}
+      return data.results[0];
+    })();
+  }
+};
 
 function getPosition() {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            resolve(position)
-        }, onError);
-    })
-}
-function onSuccess(position) {
-
-    var element = document.getElementById('geolocation');
-    element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
-        'Longitude: ' + position.coords.longitude + '<br />' +
-        'Altitude: ' + position.coords.altitude + '<br />' +
-        'Accuracy: ' + position.coords.accuracy + '<br />' +
-        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
-        'Heading: ' + position.coords.heading + '<br />' +
-        'Speed: ' + position.coords.speed + '<br />' +
-        'Timestamp: ' + position.timestamp + '<br />';
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(position => {
+      resolve(position);
+    }, onError);
+  });
 }
 function onError(error) {
-    alert('code: ' + error.code + '\n' +
-        ' message: ' + error.message + '\n');
+  alert("code: " + error.code + "\n" + " message: " + error.message + "\n");
 
-    throw Error("Couldn't retrieve location")
+  throw Error("Couldn't retrieve location");
 }
